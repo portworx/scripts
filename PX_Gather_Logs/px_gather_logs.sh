8ba7a8db2ab60653197f769eac4421f5b91ef8e2
sathish-px <ssengottuvel+github@purestorage.com>
2025-03-18 10:18:27 +0530

    Update px_gather_logs.sh
    Adding folder

--- PX_Gather_Logs/px_gather_logs.sh
+++ PX_Gather_Logs/px_gather_logs.sh
@@ -369,7 +369,7 @@ if [[ "$option" == "PX" ]]; then
 
   main_dir="PX_${namespace}_outputs_$(date +%Y%m%d_%H%M%S)"
   output_dir="/tmp/${main_dir}"
-  sub_dir=(${output_dir}/logs ${output_dir}/px_out ${output_dir}/k8s_px ${output_dir}/k8s_oth ${output_dir}/migration ${output_dir}/k8s_bkp)
+  sub_dir=(${output_dir}/logs ${output_dir}/px_out ${output_dir}/k8s_px ${output_dir}/k8s_oth ${output_dir}/migration ${output_dir}/k8s_bkp ${output_dir}/k8s_pxb)
 else
   commands=(
     "get pods -o wide -n $namespace"
