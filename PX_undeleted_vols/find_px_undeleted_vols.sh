8e783e1315a890eb5d787865a1c7b437183d8643
sathish-px <ssengottuvel+github@purestorage.com>
2025-06-24 10:09:18 +0530

    Update find_px_undeleted_vols.sh with contaniner name
    Update find_px_undeleted_vols.sh with container name

--- PX_undeleted_vols/find_px_undeleted_vols.sh
+++ PX_undeleted_vols/find_px_undeleted_vols.sh
@@ -8,7 +8,7 @@ k8s_pvs=$(kubectl get pv -o jsonpath='{.items[*].metadata.name}')
 read -ra k8s_pv_array <<< "$k8s_pvs"
 
 # Fetch list of volume names from pxctl
-px_vols=$(kubectl -n ${namespace} exec service/portworx-service -- /opt/pwx/bin/pxctl volume list -j | jq -r '.[] | "\(.locator.name)"')
+px_vols=$(kubectl -n ${namespace} exec service/portworx-service -c portworx -- /opt/pwx/bin/pxctl volume list -j | jq -r '.[] | "\(.locator.name)"')
 
 # Initialize missing list
 echo "Volumes in pxctl but NOT in Kubernetes:"
@@ -17,6 +17,6 @@ echo "VolName - VolID - VolState - VolStatus - Parentvol(ifsnap) - CreatedTime -
 
 for vol in $px_vols; do
   if [[ ! " ${k8s_pv_array[@]} " =~ " ${vol} " ]]; then
-          kubectl -n ${namespace} exec service/portworx-service -- /opt/pwx/bin/pxctl volume inspect $vol --json | jq -r '.[] | "\(.locator.name) - \(.id) - \(.state) - \(.status) - \(.source.parent) - \(.ctime) - \(.attach_time) - \(.detach_time) - \(.format) - \(.attached_on) - \(.attached_state)"'
+          kubectl -n ${namespace} exec service/portworx-service -c portworx -- /opt/pwx/bin/pxctl volume inspect $vol --json | jq -r '.[] | "\(.locator.name) - \(.id) - \(.state) - \(.status) - \(.source.parent) - \(.ctime) - \(.attach_time) - \(.detach_time) - \(.format) - \(.attached_on) - \(.attached_state)"'
   fi
 done
